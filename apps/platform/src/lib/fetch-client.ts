3c52eb6e95978fb4343e9487e66b1231a1827c5b
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2025-12-17 18:52:03 +0530

    remove commented error checks for environment variables

--- apps/platform/src/lib/fetch-client.ts
+++ apps/platform/src/lib/fetch-client.ts
@@ -6,12 +6,12 @@ export const serverIdentity = process.env.NEXT_PUBLIC_SERVER_IDENTITY;
 export const baseServerUrl = process.env.NEXT_PUBLIC_BASE_SERVER_URL;
 export const baseMailServerUrl = process.env.NEXT_PUBLIC_BASE_MAIL_SERVER_URL;
 
-if (!serverIdentity) {
-  throw new Error("Missing environment variables for server identity");
-}
-if (!baseServerUrl) {
-  throw new Error("Missing environment variables for base server URL");
-}
+// if (!serverIdentity) {
+//   throw new Error("Missing environment variables for server identity");
+// }
+// if (!baseServerUrl) {
+//   throw new Error("Missing environment variables for base server URL");
+// }
 export const baseUrl = getWindowOrigin();
 
 export const authHeaders = {
--- apps/platform/src/lib/fetch-server.ts
+++ apps/platform/src/lib/fetch-server.ts
@@ -4,9 +4,7 @@ const baseServerUrl = process.env.BASE_SERVER_URL;
 const baseMailServerUrl = process.env.BASE_MAIL_SERVER_URL;
 const serverIdentity = process.env.SERVER_IDENTITY;
 
-if (!baseServerUrl || !serverIdentity || !baseMailServerUrl) {
-  throw new Error("Missing environment variables");
-}
+
 
 /**
  *  a fetch instance to communicate with the server with the necessary headers
