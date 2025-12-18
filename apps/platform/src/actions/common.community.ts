f8b6d058b6ef464c6d71131979cb972d13bc6aba
pro-utkarshM <118096976+pro-utkarshM@users.noreply.github.com>
2025-12-18 15:28:33 -0500

    fix: Change zod import to namespace import
    - Change 'import { z }' to 'import * as z'
    - Fixes z.infer type error
    - Matches import style in community model

--- apps/platform/src/actions/common.community.ts
+++ apps/platform/src/actions/common.community.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
-import { z } from "zod";
+import * as z from "zod";
 import { awardPoints } from "./points";
 import { auth } from "~/auth";
 import {
